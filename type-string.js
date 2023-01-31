4ea9913aebc6301d4d834d95430b6581eefbfccd
gustavvoferrari <gustavvoferrari@gmail.com>
2023-01-30 21:04:54 -0300

    exemplos da aula 1 sobre fundamentos do JavaScript

---
+++ type-string.js
@@ -0,0 +1,17 @@
+const texto1 = "Olá Mundo";
+const texto2 = 'Olá mundo';
+
+const senha = "senha12243*!!!";
+
+const citacao = "o fulado disse: 'olá mundo'";
+
+const citacao2 = 'O fulado disse: "olá mundo"';
+
+console.log(citacao2);
+
+// contatenação (+) 
+
+const nome = 'nome';
+const sobreNome = 'sobrenome';
+
+console.log(nome + sobreNome);  
