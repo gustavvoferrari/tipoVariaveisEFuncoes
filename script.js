11f62db31598ca4176918b3c9cb4aaee3fca74bc
gustavvoferrari <gustavvoferrari@gmail.com>
2023-01-31 12:31:26 -0300

    Trabalhando com Var, let e const

---
+++ script.js
@@ -0,0 +1,34 @@
+let forma = 'retangulo';
+let altura = 7;
+let comprimento = 5;
+let area; 
+
+
+if (forma === 'retangulo') {
+    area = altura * comprimento;
+} else {
+    area = altura * comprimento / 2;
+}
+console.log(area);
+
+if (altura != comprimento) {
+    area = altura + comprimento;
+} else {
+    area = altura - comprimento;
+}
+
+console.log(area);
+
+
+const formato = "retangulo";
+const Altura = 7;
+const Comprimento = 5;
+let Area; 
+
+if (formato === "triangulo") {
+    Area = Altura * Comprimento;
+} else {
+    Area = Altura * Comprimento / 2;
+}
+
+console.log(Area);
