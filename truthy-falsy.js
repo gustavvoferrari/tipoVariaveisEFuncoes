9df1744e8537d162a28b186da54933b7a04fac5c
gustavvoferrari <gustavvoferrari@gmail.com>
2023-02-03 20:34:37 -0300

    truthy e falsy trabalhando

---
+++ truthy-falsy.js
@@ -0,0 +1,26 @@
+// boolean 
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+
+// truthy ou false 
+
+// 0 => false; 
+//1 => true; 
+
+console.log(0 == false); 
+console.log("" == false); 
+console.log( 1 == true);
+
+//null ==> vazio ou nada. 
+
+let numero = 3;
+let texto = "lalalaalalala";
+let varNull = null;
+
+console.log(typeof usuarioLogado);
+console.log(typeof contaPaga);
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof null);
