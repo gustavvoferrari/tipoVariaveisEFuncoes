18ab37c9ec08f1a3bd3f060e2c53207a3abcfd83
gustavvoferrari <gustavvoferrari@gmail.com>
2023-02-06 22:01:03 -0300

    testando

--- conversoes.js
+++ conversoes.js
@@ -9,3 +9,14 @@
 console.log(numero == numeroString);
 
 //conversão explicita: 
+
+
+const nome = "gustavo";
+const idade = 30;
+const cidade = "Piracicaba";
+
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos 
+e moro em ${cidade}`; 
+
+console.log(apresentacao);
---
+++ funcoes.js
@@ -0,0 +1,18 @@
+const idade = 18;
+
+const nome = "Gustavo";
+
+const cidade = "piracicaba";
+
+const x = 4+4;
+
+function testando(texto){
+    console.log(texto);
+}
+
+function algumTexto(){
+    return console.log(`Isso é algum texto, meu nome é: ${nome}, minha idade é: ${idade} e moro na cidade ${cidade}.`)
+}
+
+
+algumTexto();
